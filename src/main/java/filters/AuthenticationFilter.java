ed5ce20ff7ff8ce7368582a2ee5286c51ff5d209
Viktor Kulikov <vikulikov96@gmail.com>
2018-04-19 19:12:13 +0300

    Comment it because it works bad with ajax. Return when problem will be solved

--- src/main/java/filters/AuthenticationFilter.java
+++ src/main/java/filters/AuthenticationFilter.java
@@ -1,40 +1,42 @@
-package filters;
+// BREAKS AJAX
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
-
-@WebFilter(filterName = "AuthenticationFilter", urlPatterns = {"/*"})
-public class AuthenticationFilter implements Filter {
-    private FilterConfig config;
-
-    @Override
-    public void init(FilterConfig config) {
-        this.config = config;
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String uri = httpRequest.getRequestURI();
-        HttpSession session = httpRequest.getSession(false);
-
-        if ((session == null || session.getAttribute("user") == null) &&
-                !(uri.equals("/") || uri.equals("/registration"))) {
-            ((HttpServletResponse)response).sendRedirect("/");
-        } else {
-            chain.doFilter(request, response);
-        }
-    }
-
-    @Override
-    public void destroy() {
-        config = null;
-    }
-
-}
+//package filters;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.IOException;
+//
+//
+//@WebFilter(filterName = "AuthenticationFilter", urlPatterns = {"/profile?*"})
+//public class AuthenticationFilter implements Filter {
+//    private FilterConfig config;
+//
+//    @Override
+//    public void init(FilterConfig config) {
+//        this.config = config;
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        String uri = httpRequest.getRequestURI();
+//        HttpSession session = httpRequest.getSession(false);
+//
+//        if ((session == null || session.getAttribute("user") == null) &&
+//                !(uri.equals("/") || uri.equals("/registration"))) {
+//            ((HttpServletResponse)response).sendRedirect("/");
+//        } else {
+//            chain.doFilter(request, response);
+//        }
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        config = null;
+//    }
+//
+//}
