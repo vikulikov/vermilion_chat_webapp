9713295bd3a5387ee20808c74a832c528a575494
Viktor Kulikov <vikulikov96@gmail.com>
2018-04-19 19:10:26 +0300

    Add general servlet to handle user's actions such as logging out, deleting an account, becoming a friend with someone etc

---
+++ src/main/java/servlets/UserActionHandler.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import model.User;
+import model.UserService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet(name = "UserActionHandler", urlPatterns = {"/useraction"})
+public class UserActionHandler extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String actionValue = request.getParameter("action");
+        HttpSession session = request.getSession();
+
+        if (actionValue.equals("delete")) {
+            Map<String, User> authorizedUsers = UserService.getInstance().getSessionIdToUser();
+            authorizedUsers.remove(session.getId());
+            session.removeAttribute("user");
+            response.sendRedirect("/");
+        }
+    }
+}
